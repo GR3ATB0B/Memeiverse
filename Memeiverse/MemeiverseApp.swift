9e5219cd5ff2d1a4cf1cba697b19f0bfa23b98d9
Nash Vogeltanz <nashvogeltanz@icloud.com>
2025-11-03 20:37:14 -0500

    Initial Commit

---
+++ Memeiverse/MemeiverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeiverseApp.swift
+//  Memeiverse
+//
+//  Created by Nash Vogeltanz on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MemeiverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
